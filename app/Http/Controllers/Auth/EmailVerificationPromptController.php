70bd8a705bea7a83b4cbae306c67722d8cb84967
agungstd <apakatadunia11@gmail.com>
2025-03-20 08:53:50 +0700

    Add return type declarations and improve docblock in EmailVerificationPromptController

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class EmailVerificationPromptController extends Controller
 {
@@ -12,9 +14,9 @@ class EmailVerificationPromptController extends Controller
      * Display the email verification prompt.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return mixed
+     * @return \Illuminate\View\View|\Illuminate\Http\RedirectResponse
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): View|RedirectResponse
     {
         return $request->user()->hasVerifiedEmail()
                     ? redirect()->intended(RouteServiceProvider::HOME)
