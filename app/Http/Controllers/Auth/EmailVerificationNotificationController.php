e6a59975121e860b2ee324b40c0ddb40a1787aac
agungstd <apakatadunia11@gmail.com>
2025-03-20 08:51:34 +0700

    Added JSON response API support and verify method for email verification process.

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class EmailVerificationNotificationController extends Controller
 {
@@ -12,16 +13,43 @@ class EmailVerificationNotificationController extends Controller
      * Send a new email verification notification.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(RouteServiceProvider::HOME);
+            return $request->wantsJson()
+                ? new JsonResponse('', 204)
+                : redirect()->intended(RouteServiceProvider::HOME);
         }
 
         $request->user()->sendEmailVerificationNotification();
 
-        return back()->with('status', 'verification-link-sent');
+        return $request->wantsJson()
+            ? new JsonResponse(['message' => 'Verification link sent!'], 200)
+            : back()->with('status', 'verification-link-sent');
+    }
+    
+    /**
+     * Mark the authenticated user's email address as verified.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function verify(Request $request)
+    {
+        if ($request->user()->hasVerifiedEmail()) {
+            return $request->wantsJson()
+                ? new JsonResponse('', 204)
+                : redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
+        }
+
+        if ($request->user()->markEmailAsVerified()) {
+            $request->session()->flash('verified', true);
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse('', 204)
+            : redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
     }
 }
